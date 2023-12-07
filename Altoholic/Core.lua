dba7e2d5d32e29d11c2755d257cde5715a162a23
Thaoky <77798624+Thaoky@users.noreply.github.com>
2023-12-07 18:53:38 +0100

    Commit 10.2.008

--- Altoholic/Core.lua
+++ Altoholic/Core.lua
@@ -4,9 +4,9 @@ _G[addonName] = LibStub("AceAddon-3.0"):NewAddon(addonName, "AceConsole-3.0", "A
 
 local addon = _G[addonName]
 
-addon.Version = "v10.2.007"
+addon.Version = "v10.2.008"
 -- addon.VersionNum = 902006
-addon.VersionNum = 1002007
+addon.VersionNum = 1002008
 
 local L = LibStub("AceLocale-3.0"):GetLocale(addonName)
 local commPrefix = addonName
