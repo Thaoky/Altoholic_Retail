ecec886d5917bb9909a00ec003d2ba7d8254be99
Thaoky <77798624+Thaoky@users.noreply.github.com>
2024-09-15 17:05:42 +0200

    Release 11.0.004

--- Altoholic/Core.lua
+++ Altoholic/Core.lua
@@ -1,9 +1,9 @@
 local addonName, addon = ...
 _G[addonName] = addon
 
-addon.Version = "v11.0.003"
+addon.Version = "v11.0.004"
 -- addon.VersionNum = 11 00 006
-addon.VersionNum = 1100003
+addon.VersionNum = 1100004
 
 LibStub("LibMVC-1.0"):Embed(addon)
 
@@ -13,63 +13,7 @@ local commPrefix = addonName
 
 AddonFactory:AddKeyBinding("ALTOHOLIC", addonName, "Altoholic - Toggle UI")
 
-addon.Colors = {
-	white	= "|cFFFFFFFF",
-	red = "|cFFFF0000",
-	darkred = "|cFFF00000",
-	green = "|cFF00FF00",
-	orange = "|cFFFF7F00",
-	yellow = "|cFFFFFF00",
-	gold = "|cFFFFD700",
-	teal = "|cFF00FF9A",
-	cyan = "|cFF1CFAFE",
-	lightBlue = "|cFFB0B0FF",
-	battleNetBlue = "|cff82c5ff",
-	grey = "|cFF909090",
-	
-	-- classes
-	classMage = "|cFF69CCF0",
-	classHunter = "|cFFABD473",
-	
-	-- recipes
-	recipeGrey = "|cFF808080",
-	recipeGreen = "|cFF40C040",
-	recipeOrange = "|cFFFF8040",
-	
-	-- rarity : http://wow.gamepedia.com/Quality
-	common = "|cFFFFFFFF",
-	uncommon = "|cFF1EFF00",
-	rare = "|cFF0070DD",
-	epic = "|cFFA335EE",
-	legendary = "|cFFFF8000",
-	heirloom = "|cFFE6CC80",
-
-	Alliance = "|cFF2459FF",
-	Horde = "|cFFFF0000"
-}
-
-local colors = addon.Colors
-
-addon.Icons = {
-	ready = "\124TInterface\\RaidFrame\\ReadyCheck-Ready:14\124t",
-	waiting = "\124TInterface\\RaidFrame\\ReadyCheck-Waiting:14\124t",
-	notReady = "\124TInterface\\RaidFrame\\ReadyCheck-NotReady:14\124t",
-	questionMark = "Interface\\RaidFrame\\ReadyCheck-Waiting",
-
-	Alliance = "Interface\\Icons\\INV_BannerPVP_02",
-	Horde = "Interface\\Icons\\INV_BannerPVP_01",
-	Neutral = "Interface\\Icons\\Achievement_character_pandaren_female",
-}
-
--- Place Enums in a separate file when there are enough to justify it
-addon.Enum = {
-	ArmorTypes = {
-		[1] = C_Item.GetItemSubClassInfo(Enum.ItemClass.Armor, Enum.ItemArmorSubclass.Cloth), -- "Cloth"
-		[2] = C_Item.GetItemSubClassInfo(Enum.ItemClass.Armor, Enum.ItemArmorSubclass.Leather), -- "Leather"
-		[3] = C_Item.GetItemSubClassInfo(Enum.ItemClass.Armor, Enum.ItemArmorSubclass.Mail), -- "Mail"
-		[4] = C_Item.GetItemSubClassInfo(Enum.ItemClass.Armor, Enum.ItemArmorSubclass.Plate) -- "Plate"
-	},
-}
+local colors = AddonFactory.Colors
 
 addon.TradeSkills = {
 	Recipes = {},
@@ -98,9 +42,6 @@ addon.TradeSkills = {
 		TAILORING = C_Spell.GetSpellName(3908),
 	},
 }
-
--- One empty function to rule them all
-addon.EmptyFunc = function() end
 
 -- ** LDB Launcher **
 LibStub:GetLibrary("LibDataBroker-1.1"):NewDataObject(addonName, {
