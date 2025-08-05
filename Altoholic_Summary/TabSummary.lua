20db649f8c9ce3c9cbae3aed0fd9dac481e8af6b
GovtGeek <26489473+GovtGeek@users.noreply.github.com>
2025-08-04 19:27:23 -0700

    Fix CurrentLevelsMax issue
    Fix for expansion max level change, mainly experienced with new expansions
    Stops the need for changing filter from all to something and back

--- Altoholic_Summary/TabSummary.lua
+++ Altoholic_Summary/TabSummary.lua
@@ -910,7 +910,7 @@ AddonFactory:OnAddonLoaded(addonTabName, function()
 		CurrentFactions = 3,							-- 1 = Alliance, 2 = Horde, 3 = Both
 		CurrentLevels = 1,							-- 1 = All
 		CurrentLevelsMin = 1,							
-		CurrentLevelsMax = 80,					
+		CurrentLevelsMax = GetMaxLevelForExpansionLevel(GetExpansionLevel()),
 		CurrentBankType = 0,							-- 0 = All
 		CurrentClasses = 0,							-- 0 = All
 		CurrentRaces = 0,								-- 0 = All
@@ -924,8 +924,13 @@ AddonFactory:OnAddonLoaded(addonTabName, function()
 	})
 	options = Altoholic_SummaryTab_Options
 	options.CurrentRaces = options.CurrentRaces or 0
-	
+
+	if options.OPTION_LEVELS == 1 then 
+		options.CurrentLevelsMax = GetMaxLevelForExpansionLevel(GetExpansionLevel())
+	end
+
 	-- Update only when options are ready
 	tab.CategoriesList.Entry2:Button_OnClick("LeftButton")
 	tab:Update()
 end)
+
