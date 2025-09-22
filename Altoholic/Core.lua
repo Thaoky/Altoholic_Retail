5552af505b974cfb73ca28128559d78c6b9da97d
Thaoky <77798624+Thaoky@users.noreply.github.com>
2025-09-22 18:37:26 +0200

    Release 11.2.006

--- Altoholic/Core.lua
+++ Altoholic/Core.lua
@@ -1,9 +1,9 @@
 local addonName, addon = ...
 _G[addonName] = addon
 
-addon.Version = "v11.2.005"
+addon.Version = "v11.2.006"
 -- addon.VersionNum = 11 00 007
-addon.VersionNum = 1102005
+addon.VersionNum = 1102006
 
 LibStub("LibMVC-1.0"):Embed(addon)
 
